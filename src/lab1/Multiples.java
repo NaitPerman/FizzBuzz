c4412c6d5a8324f8e258dc9484c0e6117911f6f2
Nathan Fermin <nathan.fermin@mail.utoronto.ca>
2025-09-09 14:42:42 -0400

    This is the end of the Lab, I'm sending over my Multiples file.

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,4 +1,38 @@
 package lab1;
 
 public class Multiples {
+    public static void main(String[] args) {
+        System.out.println("There are " + multiples(1000, 5, 3));
+
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int fin = 0;
+        int i = 1;
+        while (i < n) {
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % a == 0;
+            boolean divisibleBy5 = i % b == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
+
+                fin++;
+
+            } else if (divisibleBy3) {
+
+                fin++;
+
+            } else if (divisibleBy5) {
+
+                fin++;
+
+            }
+
+
+            i++;
+        }
+        return fin;
+    }
+
 }
